c6cc7df91833d237d4e53cb15808c53e67d45cdd
Oleg <ironmank47@gmail.com>
2024-10-01 16:47:42 +0300

    add task 4.2

---
+++ homework_4/task_2_checking_the_number/script.js
@@ -0,0 +1,15 @@
+const number = +prompt("Введіть тризначне число:", '');
+
+let numberOne = Math.floor(number / 100 % 10)
+let numberTwo = Math.floor(number / 10 % 10)
+let numberThree = Math.floor(number % 10)
+
+console.log(`${numberOne} ${numberTwo} ${numberThree}`);
+
+if (numberOne == numberTwo && numberTwo == numberThree) {
+    console.log(`Так, всі цифри однакові`);
+} else if (numberOne == numberThree || numberTwo == numberThree || numberOne == numberTwo) {
+    console.log(`Е серед цифр однакові цифри`)
+} else {
+    console.log("Всі цифри різні")
+}
