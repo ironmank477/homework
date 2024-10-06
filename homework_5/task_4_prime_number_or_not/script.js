058e1f3070947d71555875d1094a1f8905c179da
Oleg <ironmank47@gmail.com>
2024-10-06 12:16:07 +0300

    changed script file

--- homework_5/task_4_prime_number_or_not/script.js
+++ homework_5/task_4_prime_number_or_not/script.js
@@ -1,21 +1,15 @@
 const value = +prompt("Pleas enter a number:", '');
 let res = ''
+let count = 0
 
-
-switch (true) {
-    case (value % 10 === 4 || value % 10 === 6 || value % 10 === 8 || value % 10 === 0 || value === 21 || value === 27 || value === 33 || value === 39):
-        res = `${value}`
-        console.log(`Число:${res} не просте`)
-        break;
-    case (value === 2 || value === 5 || value % 10 === 1 || value % 10 === 3 || value % 10 === 7 || value % 10 === 9):
-        res = `${value}`
-        console.log(`Число:${res} просте`)
-        break;
-    case (value % 10 === 2 || value % 10 === 5):
-        res = `${value}`
-        console.log(`Число:${res} НЕ просте`)
-        break;
-    default:
-        console.log(`Введено число: ${res}`)
+for (let i = 1; i <= value; i++) {
+    if (value % i === 0) {
+        res += `${i} `;
+        count++
+    }
 }
-
+if (count === 2) {
+    console.log(`${res} Просте`)
+} else {
+    console.log(`Не просте ${res}  дільників: ${count}`)
+}
